05b29d24e0971d04873b0ee6ebc16eb90bb04f3a
Taylor Stib <tstib3@gmail.com>
2014-07-09 10:41:57 -0400

    Friend scores show on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,13 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
   def index
   	@score = Score.find_by fight_id: 1, user_id: current_user.id
-    @users = User.all
+    # @users = User.all
+    @user = current_user
+    @friendids = @user.friendships.select(:friend_id)
+    @friendscore = Score.where(user_id: @friendids, fight_id: 1)
+    @output = @friendscore.pluck(:r1_f1, :r1_f2, :r2_f1, :r2_f2, :r3_f1, :r3_f2)
+    @emailarr = @user.friends.pluck(:email)
+
   end
 
   def show
